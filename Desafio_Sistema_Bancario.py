62b6524c0e7e508da2eb1409de39665d6977ae88
mhmartins <murilohumbertomartins@gmail.com>
2025-09-26 20:25:57 -0300

    Adicionado o sistema de cédulas

--- Desafio_Sistema_Bancario.py
+++ Desafio_Sistema_Bancario.py
@@ -41,6 +41,36 @@ while True:
             saldo -= valor
             extrato += f'Saque no valor de R$ {valor:.2f}\n'
             número_de_saques += 1
+            c50 = c20 = c10 = c5 = c1 = 0
+            v = valor
+            while True:
+                if v >= 50:
+                    v -= 50
+                    c50 += 1
+                else:
+                    if v >= 20:
+                        v -= 20
+                        c20 += 1
+                    else:
+                        if v >= 10:
+                            v -= 10
+                            c10 += 1
+                        else:
+                            if v >= 5:
+                                v -= 5
+                                c5 += 1
+                            else:
+                                if v >= 1:
+                                    v -= 1
+                                    c1 += 1
+                                else:
+                                    break
+            print(f'{'Serão sacadas':=^20}')
+            print(f'{c50} cédulas de R$ 50.00')
+            print(f'{c20} cédulas de R$ 20.00')
+            print(f'{c10} cédulas de R$ 10.00')
+            print(f'{c5} cédulas de R$ 5.00')
+            print(f'{c1} cédulas de R$ 1.00')
         else:
             print('Operação falhou! O valor inválido, tente novamente.')
     elif opção == 'e':
